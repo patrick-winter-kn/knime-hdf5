cdb680275783411804ea8a564cd1d552ec9b15c1
Markimk13 <markus.koehler@uni-konstanz.de>
2018-04-13 16:39:07 +0200

    Added Drag-and-Drop support for selection in JTree from JList; Tried to change icons in JTree

--- org.knime.hdf5/src/org/knime/hdf5/nodes/writer/HDF5WriterNodeDialog.java
+++ org.knime.hdf5/src/org/knime/hdf5/nodes/writer/HDF5WriterNodeDialog.java
@@ -1,18 +1,29 @@
 package org.knime.hdf5.nodes.writer;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Component;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.activation.UnsupportedDataTypeException;
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
+import javax.swing.DropMode;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTree;
+import javax.swing.TransferHandler;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
@@ -40,9 +51,14 @@ class HDF5WriterNodeDialog extends DefaultNodeSettingsPane {
 	
 	private SettingsModelString m_groupNameSettings;
 
-	private DefaultListModel<DataColumnSpec> m_dataSetSpecModel = new DefaultListModel<>();
+	private DefaultListModel<DataColumnSpec> m_columnSpecModel = new DefaultListModel<>();
 	
-	private DefaultListModel<DataColumnSpec> m_attributeSpecModel = new DefaultListModel<>();
+	private DefaultListModel<DataColumnSpec> m_flowVariableSpecModel = new DefaultListModel<>();
+	
+	// TODO this should be in the SettingsFactory later
+	private Map<String, List<DataColumnSpec>> m_transfer = new HashMap<>();
+	
+	private static final String COL_TRANSFER = "colSpecs";
 	
     /**
      * Creates a new {@link NodeDialogPane} for the column filter in order to
@@ -58,10 +74,33 @@ class HDF5WriterNodeDialog extends DefaultNodeSettingsPane {
 		JPanel select = new JPanel();
 		panel.add(select);
 
-		JList<DataColumnSpec> colList = new JList<>(m_dataSetSpecModel);
+		JList<DataColumnSpec> colList = new JList<>(m_columnSpecModel);
+		colList.setVisibleRowCount(-1);
+		colList.setDragEnabled(true);
+		colList.setTransferHandler(new TransferHandler() {
+
+			private static final long serialVersionUID = -4233815652319877595L;
+
+            public int getSourceActions(JComponent c) {
+                return COPY;
+            }
+             
+            @SuppressWarnings("unchecked")
+			protected Transferable createTransferable(JComponent c) {
+                if (c instanceof JList) {
+                	JList<DataColumnSpec> list = (JList<DataColumnSpec>) c;
+    				m_transfer.put(COL_TRANSFER, list.getSelectedValuesList());
+					
+					return new StringSelection(COL_TRANSFER);
+                }
+                return null;
+            }
+		});
 		select.add(colList);
         
-		JList<DataColumnSpec> varList = new JList<>(m_attributeSpecModel);
+		JList<DataColumnSpec> varList = new JList<>(m_flowVariableSpecModel);
+		varList.setVisibleRowCount(-1);
+		varList.setDragEnabled(true);
 		select.add(varList);
         
 		JPanel output = new JPanel();
@@ -69,35 +108,107 @@ class HDF5WriterNodeDialog extends DefaultNodeSettingsPane {
 		
 		DefaultMutableTreeNode root = new DefaultMutableTreeNode("group");
 		JTree tree = new JTree(root);
-		tree.setDragEnabled(true);
 		output.add(tree);
-        
-		JButton createDsButton = new JButton("createDs");
-		createDsButton.addActionListener(new ActionListener() {
+		/*
+		tree.setCellRenderer(new DefaultTreeCellRenderer() {
 
+			private static final long serialVersionUID = -2424225988962935310L;
+
+			
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				DefaultMutableTreeNode newChild = new DefaultMutableTreeNode("dataSet[" + root.getChildCount() + "]");
-				root.add(newChild);
-				((DefaultTreeModel) (tree.getModel())).reload();
-				tree.makeVisible(new TreePath(newChild.getPath()));
+			public Component getTreeCellRendererComponent(final JTree tree,
+					final Object value, final boolean sel, final boolean expanded,
+					final boolean leaf, final int row, final boolean hasfocus) {
+				super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row,
+						hasfocus);
+				
+				if (value instanceof DefaultMutableTreeNode) {
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+					String name = (String) node.getUserObject();
+					
+					Icon icon = null;
+					if (name.equals("group")) {
+						icon = new ImageIcon("icons/group.png");
+					} else if (name.startsWith("dataSet")) {
+						icon = new ImageIcon("icons" + File.separator + "dataSet.png");
+					} else {
+						icon = new ImageIcon("icons" + File.separator + "column.png");
+					}
+					setLeafIcon(icon);
+					setClosedIcon(icon);
+					setOpenIcon(icon);
+				} else {
+					// TODO exception
+				}
+				return this;
 			}
 		});
-		output.add(createDsButton);
-		
-		JButton addToDsButton = new JButton("addToDs");
-		addToDsButton.addActionListener(new ActionListener() {
+		*/
+		tree.setDragEnabled(true);
+        tree.setTransferHandler(new TransferHandler() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				DefaultMutableTreeNode newChild = new DefaultMutableTreeNode(colList.getSelectedValue());
-				newChild.setAllowsChildren(false);
-				((DefaultMutableTreeNode) tree.getSelectionPath().getLastPathComponent()).add(newChild);
+			private static final long serialVersionUID = -4233815652319877595L;
+        	
+			public boolean canImport(TransferHandler.TransferSupport info) {
+				// we only import Strings
+                if (!info.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    return false;
+                }
+                
+                JTree.DropLocation dl = (JTree.DropLocation) info.getDropLocation();
+                if (dl.getPath() == null) {
+                    return false;
+                }
+                return true;
+            }
+
+			public boolean importData(TransferHandler.TransferSupport info) {
+                if (!info.isDrop()) {
+                    return false;
+                }
+
+                // Check for String flavor
+                if (!info.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    return false;
+                }
+                
+                JTree.DropLocation dl = (JTree.DropLocation) info.getDropLocation();
+                TreePath path = dl.getPath();
+                DefaultMutableTreeNode parent = (DefaultMutableTreeNode) path.getLastPathComponent();
+                
+                if (parent.isRoot()) {
+                	DefaultMutableTreeNode newChild = new DefaultMutableTreeNode("dataSet[" + parent.getChildCount() + "]");
+                	parent.add(newChild);
+                	path = path.pathByAddingChild(newChild);
+                	parent = newChild;
+                	
+                } else if (!parent.getAllowsChildren()) {
+                	path = path.getParentPath();
+                	parent = (DefaultMutableTreeNode) parent.getParent();
+                }
+ 
+                // Get the string that is being dropped.
+                Transferable t = info.getTransferable();
+                List<DataColumnSpec> data = null;
+                try {
+                    data = m_transfer.get((String) t.getTransferData(DataFlavor.stringFlavor));
+                } catch (Exception e) {
+                	return false;
+                }
+                
+                for (int i = 0; i < data.size(); i++) {
+                    DefaultMutableTreeNode newChild = new DefaultMutableTreeNode(data.get(i));
+    				newChild.setAllowsChildren(false);
+    				parent.add(newChild);
+                }
+				
 				((DefaultTreeModel) (tree.getModel())).reload();
-				tree.makeVisible(new TreePath(newChild.getPath()));
-			}
-		});
-		output.add(addToDsButton);
+				tree.makeVisible(path.pathByAddingChild(parent.getFirstChild()));
+                
+                return true;
+            }
+        });
+        tree.setDropMode(DropMode.ON_OR_INSERT);
 	}
     
 	private void createFileChooser() {
@@ -140,13 +251,13 @@ class HDF5WriterNodeDialog extends DefaultNodeSettingsPane {
 	public void loadAdditionalSettingsFrom(final NodeSettingsRO settings,
             final DataTableSpec[] specs) throws NotConfigurableException {
     	for (int i = 0; i < specs[0].getNumColumns(); i++) {
-        	m_dataSetSpecModel.addElement(specs[0].getColumnSpec(i));
+        	m_columnSpecModel.addElement(specs[0].getColumnSpec(i));
     	}
 
     	FlowVariable[] flowVariables = getAvailableFlowVariables().values().toArray(new FlowVariable[] {});
 		for (FlowVariable flowVariable : flowVariables) {
 			try {
-				m_attributeSpecModel.addElement(new DataColumnSpecCreator(flowVariable.getName(), Hdf5KnimeDataType.getColumnDataType(flowVariable.getType())).createSpec());
+				m_flowVariableSpecModel.addElement(new DataColumnSpecCreator(flowVariable.getName(), Hdf5KnimeDataType.getColumnDataType(flowVariable.getType())).createSpec());
 				
 			} catch (UnsupportedDataTypeException udte) {
 				NodeLogger.getLogger("HDF5 Files").error("Type of FlowVariable \"" + flowVariable.getName() + "\" is not supported", udte);
